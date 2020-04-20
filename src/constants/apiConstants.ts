8a968e34daf6d42459339d509eade03ea97b6d9b
andri95 <33896363+andri95@users.noreply.github.com>
2020-04-20 17:26:38 +0000

    Update apiConstants.ts

--- src/constants/apiConstants.ts
+++ src/constants/apiConstants.ts
@@ -1,4 +1,4 @@
 export const PORT: Number = parseInt(`${process.env.PORT}`, 10) || 3000
 export const apiPath: string = '/api/';
 export const version: string = 'v1';
-export const URI: string = 'mongodb+srv://andrifannarp:andri7649@cluster0-3t3xi.mongodb.net/test?retryWrites=true&w=majority';
+export const URI: string = 'mongodb+srv://andrifannarp:<password>@cluster0-3t3xi.mongodb.net/test?retryWrites=true&w=majority';
