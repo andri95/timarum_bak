f723a672515a25d26101fadc77871a5b3f610c49
Andri Pétursson <afannar@gmail.com>
2020-04-22 14:45:13 +0000

    yiss

--- src/constants/apiConstants.ts
+++ src/constants/apiConstants.ts
@@ -1,5 +1,5 @@
 export const PORT: Number = parseInt(`${process.env.PORT}`, 10) || 3000
 export const apiPath: string = '/api/';
 export const version: string = 'v1';
-export const URI: string = 'mongodb+srv://andrifannarp:andri7649@cluster0-3t3xi.mongodb.net/timarum?retryWrites=true&w=majority';
+export const URI: string = 'mongodb+srv://andrifannarp:<password>@cluster0-3t3xi.mongodb.net/timarum?retryWrites=true&w=majority';
 export const allowedOrigins: Array<string> = ['http://localhost:3000', 'http://www.timarum.is'];
